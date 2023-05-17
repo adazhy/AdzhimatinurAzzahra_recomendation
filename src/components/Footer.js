8f32ae7b831149310902694a36bc9061106779b6
seriani0707 <adzhimatinurazzahra@gmail.com>
2023-05-17 16:54:20 +0700

    Task 7

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+/**
+ * Membuat component Footer.
+ * Component Footer menampilkan footer website.
+ * @returns
+ */
+function Footer() {
+  return (
+    <footer>
+      <h2>Copyright by Adzhimatinur Azzahra</h2>
+      <p>ReactJS</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import App from "./App";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+
+// Import Browser Router dari React Router
+import { BrowserRouter } from "react-router-dom";
+
+// get root element
+const container = document.getElementById("root");
+
+// create root
+const root = ReactDOM.createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
+reportWebVitals();
